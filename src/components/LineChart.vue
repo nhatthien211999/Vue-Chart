ff861a28222b6d818f2ee4c0f0de2f9d589c0e17
LAPTOP-BHRL8RCA\Admin <thien.ldn5368@sinhvien.hoasen.edu.vn>
2021-02-18 14:22:26 +0700

    first commit

---
+++ src/components/LineChart.vue
@@ -0,0 +1,54 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  props: {
+    label: {
+      type: String
+    },
+    chartData: {
+      type: Array
+    },
+    options: {
+      type: Object
+    },
+    chartColors: {
+      type: Object
+    }
+  },
+  mounted() {
+
+    // https://www.chartjs.org/docs/latest/
+
+    const dates = this.chartData.map(d => d.date).reverse();
+    const totals = this.chartData.map(d => d.total).reverse();
+
+    //màu của chart
+    const {
+      borderColor,
+      pointBorderColor,
+      pointBackgroundColor,
+      backgroundColor
+    } = this.chartColors;
+
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: [
+          {
+            label: this.label, //ghi chú 
+            data: totals,   //giá trị
+            borderColor: borderColor, //màu viền
+            pointBorderColor: pointBorderColor, 
+            pointBackgroundColor: pointBackgroundColor,
+            backgroundColor: backgroundColor
+          }
+        ]
+      },
+      this.options
+    );
+  }
+};
+</script>
+
